44a40f168c69f187da6447d9c30025f07a84732f
viml kumar <vimalkg@gmail.com>
2025-03-01 14:50:52 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

--- beastify/content_scripts/beastify.js
+++ beastify/content_scripts/beastify.js
@@ -1,19 +1,6 @@
 (function() {
-  /**
-   * Check and set a global guard variable.
-   * If this content script is injected into the same page again,
-   * it will do nothing next time.
-   */
-  if (window.hasRun) {
-    return;
-  }
+  if (window.hasRun) { return; }
   window.hasRun = true;
-
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
   function insertBeast(beastURL) {
     removeExistingBeasts();
     let beastImage = document.createElement("img");
@@ -22,27 +9,39 @@
     beastImage.className = "beastify-image";
     document.body.appendChild(beastImage);
   }
-
-  /**
-   * Remove every beast from the page.
-   */
   function removeExistingBeasts() {
     let existingBeasts = document.querySelectorAll(".beastify-image");
-    for (let beast of existingBeasts) {
-      beast.remove();
-    }
+    for (let beast of existingBeasts) { beast.remove(); }
   }
-
-  /**
-   * Listen for messages from the background script.
-   * Call "beastify()" or "reset()".
-   */
-  browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "beastify") {
-      insertBeast(message.beastURL);
-    } else if (message.command === "reset") {
-      removeExistingBeasts();
-    }
+  /** * Listen for messages from the background script. * Call "beastify()" or "reset()". */
+  browser.runtime.onMessage.addListener((msgzo) => {
+	if (msgzo.command === "beastify") { 	insertBeast(msgzo.beastURL); }
+    else if (msgzo.command === "reset") { removeExistingBeasts(); }
   });
-
 })();
+/*
+0. anonymous function define n call
+1. window.hasRun
+2. dom_elem = document.createElement("img");
+3. dom_elem.setAttribute("src", beastURL);
+4. dom_elem.style.height = "100vh";
+5. dom_elem.className = "beastify-image";
+6. document.body.appendChild(dom_elem);
+7. nonlive_nodelist_of_elem =  document.querySelectorAll(selector); 
+  1. const matches = document.querySelectorAll("div.note, div.alert");
+  2. const container = document.querySelector("#test");
+  2. const matches = container.querySelectorAll("div.highlighted > p");
+8. for of loop 
+7. browser.tabs.sendMessage(tabs[0].id, msgzo);
+9. browser.runtime.onMessage.addListener((msgzo) => {})
+7. const hidePage_css_codebqs = `body > :not(.beastify-image) { display: none; }`;
+7. document.addEventListener("click", (e) => {}) 
+7. browser.runtime.getURL("beasts/frog.jpg");
+7. browser.tabs.insertCSS({code: hidePage_css_codebqs}).then() => {})
+7. browser.tabs.removeCSS({code: hidePage}).then(() => {})
+7. browser.tabs.query({active: true, currentWindow: true}).then(reset).catch(reportError)
+7. browser.tabs.executeScript({file: "/content_scripts/beastify.js"}).then(listenForClicks).catch(reportExecuteScriptError);
+7. console.error(`Could not beastify: ${error}`)
+
+ 
+*/
